1e54f9c8f5bae997b52ea6aaef3828271146d025
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2013-06-13 13:34:34 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BalleOye::Application.config.secret_token = '27c1807f90071c7cf4e8e8be7ddec99a442fc5af883cbae20f672822ceca4f1cf6b851968951066601198416b3299973780fff098f384bfd87771f356b81db0a'
